2bf6429bfdc89105352ef04cd87c13b3cbef97ea
nadareda12001 <142901780+nadareda12001@users.noreply.github.com>
2023-09-28 14:50:21 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:task/view/page1.dart';
-import 'package:sizer/sizer.dart';
+
 
 
 
@@ -13,10 +13,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return
-      // ResponsiveSizer(
-      // builder: (context, orientation, deviceType) {
-        GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'application 1',
       getPages: [
